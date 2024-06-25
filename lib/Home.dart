23c884e067de8a84aed31fd0771ae9ebfe738aad
Ybonne05 <crazydavid0101@gmail.com>
2024-06-25 12:05:56 +0800

    user profile

---
+++ lib/Home.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold  (
+      appBar: AppBar(
+        title: Text('User Profile',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Colors.pink[300],
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget> [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Center(
+                  child: CircleAvatar(
+                    child: Image.asset('assets/female.png'),
+                    radius: 100.0,
+                  ),
+                ),
+                Divider(
+                  height: 55.0,
+                  color: Colors.pink[100],
+                  thickness: 3.0,
+                ),
+                Row(
+                  children: [
+                    Icon(Icons.person_2),
+                    SizedBox(width: 3.0,),
+                    Text(
+                      'NAME :',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 2.0,
+                        height: 3.0,
+                      ),
+                    ),
+                  ],
+                ),
+                Text(
+                  'Ybonne B. Legaspi',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.0,
+                  ),
+                ),
+                SizedBox(height: 30.0,),
+                Text(
+                  'YEAR :',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.0,
+                  ),
+                ),
+                Text(
+                  '4TH YEAR',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.0,
+                  ),
+                ),
+                SizedBox(height: 30.0,),
+                Text(
+                  'Email :',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.0,
+                  ),
+                ),
+                Text(
+                  'ybonnelegaspi0823@gmail.com',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1.5,
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                ElevatedButton(
+                  onPressed: (){},
+                  child: Text('Add Year',
+                    style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStateProperty.all(Colors.pink[200]),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,77 +1,11 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutterapps/Home.dart';
+
 void main() => runApp(MaterialApp(
-    home:Scaffold  (
-     appBar: AppBar(
-       title: Text('User Profile',
-         style: TextStyle(
-         fontWeight: FontWeight.bold,
-         ),
-       ),
-       backgroundColor: Colors.pink[300],
-       centerTitle: true,
-     ),
-      body: Padding(
-          padding: EdgeInsets.all(20.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              SizedBox(height: 10.0,),
-              Text(
-                'NAME :',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 2.0,
-                  height: 3.0,
-                ),
-              ),
-              Text(
-                'Ybonne B. Legaspi',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 2.0,
-                ),
-              ),
-              SizedBox(height: 30.0,),
-              Text(
-                'YEAR :',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 2.0,
-                ),
-              ),
-              Text(
-                '4TH YEAR',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 2.0,
-                ),
-              ),
-              SizedBox(height: 30.0,),
-              Text(
-                'Email :',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 2.0,
-                ),
-              ),
-              Text(
-                'ybonnelegaspi0823@gmail.com',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 1.0,
-                ),
-              ),
-            ],
-        ),
-      ),
-    ),
-  ),
-);
+    home:Home(),
+  ));
+
+
+
 

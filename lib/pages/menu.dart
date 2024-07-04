633bc94e26465a0852ab7c0ce10e4346a85e75e0
Ybonne05 <crazydavid0101@gmail.com>
2024-07-04 11:31:13 +0800

    Activity 4

--- lib/pages/menu.dart
+++ lib/pages/menu.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutterapps/services/Product.dart';
 import 'package:flutterapps/services/MenuCard.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Menu extends StatefulWidget {
   const Menu({super.key});
@@ -15,7 +16,7 @@ class Menu extends StatefulWidget {
 class _MenuState extends State<Menu> {
   late Future<List<dynamic>> products;
   fetchData() async{
-    final response = await http.get(Uri.parse('http://192.168.192.0:8080/products')
+    final response = await http.get(Uri.parse('http://192.168.194.26:8080/products')
     );
     print(response.body);
   }
@@ -40,7 +41,46 @@ class _MenuState extends State<Menu> {
         centerTitle: true,
         backgroundColor: Colors.pink[300],
       ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(20, 20, 20, 0),
+        child: FutureBuilder(
+          future: products,
+          builder: (context, snapshots){
+            if (snapshots.connectionState == ConnectionState.waiting){
+              return Center(
+                child: SpinKitFoldingCube(
+                  color: Colors.white,
+                  size: 60.0,
+                ),
+              );
+            }
+            if (snapshots.hasData) {
+              List products = snapshots.data!;
+              return Padding(padding: EdgeInsets.all(3.0),
+                child: ListView.builder(
+                  itemCount: products.length,
+                  itemBuilder: (context, index){
+                    return Card(
+                      child: ListTile(
+                        title: Column(
+                          children: [
+                            Text(products[index].productName),
+                            Text(products[index].price.toString()),
+                          ],
+                        ),
 
+                      ),
+                    );
+                  }
+                ),
+              );
+            }
+            return Center(
+              child: Text('Unable to load data'),
+            );
+          },
+        ),
+      ),
     );
   }
 }
